8085bd66e0e349f4a497fcb32d43bc0e0b371efc
JavonCroes <145350810+JavonCroes@users.noreply.github.com>
2026-01-13 12:17:55 +0100

    Changed name of pages
    Changed the index files in the fighters and events folder to there own name and changed the routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect()->route('dashboard'));
+Route::get("/", fn() => redirect()->route("dashboard"));
 
-Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
-Route::get('/', fn () => redirect()->route('dashboard'))->name('home');
-Route::get('/fighters', fn () => view('fighters.index'))->name('fighters.index');
-Route::get('/events', fn () => view('events.index'))->name('events.index');
+Route::get("/dashboard", fn() => view("dashboard"))->name("dashboard");
+Route::get("/", fn() => redirect()->route("dashboard"))->name("home");
+Route::get("/fighters", fn() => view("fighters.fighters"))->name(
+    "fighters.index",
+);
+Route::get("/events", fn() => view("events.events"))->name("events.index");
